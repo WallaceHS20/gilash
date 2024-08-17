ff78c4a085e9518b1ee0f56835e5a6cdf781c879
wallaceHS20 <wallacehonorato67@gmail.com>
2024-08-17 18:25:06 -0300

    dados

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,6 +13,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/gilash/', // Adicione esta linha
   plugins: [
     VueRouter(),
     Layouts(),
