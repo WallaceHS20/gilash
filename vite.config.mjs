d2ed11253ef2a373a8df3563fa32ca5ff89262c8
wallaceHS20 <wallacehonorato67@gmail.com>
2024-10-31 13:05:37 -0300

    ajustes

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,6 @@
 // Plugins
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import Fonts from 'unplugin-fonts/vite'
 import Layouts from 'vite-plugin-vue-layouts'
 import Vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
@@ -15,29 +14,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gilash/', // Adicione esta linha
+  base: '/gilash/',
   plugins: [
     VueRouter(),
     Layouts(),
     Vue({
       template: { transformAssetUrls }
     }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
+
     Vuetify({
       autoImport: true,
       styles: {
         configFile: 'src/styles/settings.scss',
       },
     }),
+
     Components(),
-    Fonts({
-      google: {
-        families: [{
-          name: 'Roboto',
-          styles: 'wght@100;300;400;500;700;900',
-        }],
-      },
-    }),
     AutoImport({
       imports: [
         'vue',
@@ -48,6 +40,7 @@ export default defineConfig({
       },
       vueTemplate: true,
     }),
+
   ],
   define: { 'process.env': {} },
   resolve: {
