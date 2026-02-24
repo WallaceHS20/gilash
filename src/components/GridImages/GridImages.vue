f88d51a2704b65ebc3500b5ba2e9d59140a0806d
wallaceHS20 <wallacehonorato67@gmail.com>
2026-02-24 19:43:03 -0300

    new prices

--- src/components/GridImages/GridImages.vue
+++ src/components/GridImages/GridImages.vue
@@ -1,74 +1,51 @@
 <template>
-    <h1 class="title-grid">Venha renovar seu brilho</h1>
-    <v-card
-      class="mx-auto c-grid"
-    >
-      <v-container fluid>
-        <v-row dense>
-          <v-col
-            v-for="card in cards"
-            :key="card.title"
-            :cols="card.flex"
-          >
-            <v-card>
-              <v-img
-                :src="card.src"
-                class="align-end"
-                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                height="400px"
-                cover
-              >
-                <v-card-title class="text-white" v-text="card.title"></v-card-title>
-              </v-img>
-  
-              <v-card-actions>
-                <v-spacer></v-spacer>
-  
-                <v-btn
-                  color="red"
-                  icon="mdi-heart"
-                  size="small"
-                ></v-btn>
-  
-                <v-btn
-                  color="pink"
-                  icon="mdi-bookmark"
-                  size="small"
-                ></v-btn>
-  
-                <v-btn
-                  color="blue"
-                  icon="mdi-share-variant"
-                  size="small"
-                ></v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
-  </template>
-  <script>
-    export default {
-      data: () => ({
-        cards: [
-          { title: 'Rua Manoel Rozendo de Oliveira n° 53', src: 'https://github.com/WallaceHS20/IMAGE-REPOSITORY/blob/main/gih2offcice.PNG?raw=true', flex: 12 },
-          { title: 'Próximo da Adonai Fitness', src: 'https://github.com/WallaceHS20/IMAGE-REPOSITORY/blob/main/gih1offcice.jpg?raw=true', flex: 6 },
-          { title: 'Segunda a Sábado', src: 'https://github.com/WallaceHS20/IMAGE-REPOSITORY/blob/main/gih3offcice.jpg?raw=true', flex: 6 },
-        ],
-      }),
-    }
-  </script>
+  <h1 class="title-grid">Venha renovar seu brilho</h1>
+  <v-card class="mx-auto c-grid">
+    <v-container fluid>
+      <v-row dense>
+        <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+          <v-card>
+            <v-img :src="card.src" class="align-end" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="400px"
+              cover>
+              <v-card-title class="text-white text-break" v-text="card.title">
+              </v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn color="red" icon="mdi-heart" size="small"></v-btn>
+
+              <v-btn color="pink" icon="mdi-bookmark" size="small"></v-btn>
+
+              <v-btn color="blue" icon="mdi-share-variant" size="small"></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+<script>
+export default {
+  data: () => ({
+    cards: [
+      { title: 'Rua Manoel Rozendo de Oliveira n° 53', src: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/espelho.jpeg', flex: 12 },
+      { title: 'Próximo da Adonai Fitness', src: 'https://github.com/WallaceHS20/IMAGE-REPOSITORY/blob/main/gih1offcice.jpg?raw=true', flex: 6 },
+      { title: 'Segunda a Sábado', src: 'https://github.com/WallaceHS20/IMAGE-REPOSITORY/blob/main/gih3offcice.jpg?raw=true', flex: 6 },
+    ],
+  }),
+}
+</script>
 
 <style scoped>
-.title-grid{
-    padding: 10px;
-    text-align: center;
+.title-grid {
+  padding: 10px;
+  text-align: center;
 }
 
-.c-grid{
-    margin: 10px 0;
-    padding: 10px;
+.c-grid {
+  margin: 10px 0;
+  padding: 10px;
 }
-
 </style>
--- src/stores/product.js
+++ src/stores/product.js
@@ -1,119 +1,136 @@
 // Utilities
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useProductStore = defineStore('product', {
+export const useProductStore = defineStore("product", {
   state: () => ({
     cilios: [
       {
-        nome: 'Volume Luxo',
-        foto: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/5e.jpg',
-        nota: '5.0',
-        valor: '140.00',
-        descricao: 'Esse método proporciona um volume médio a intenso, criando um olhar marcante e glamoroso.',
-        manutencao: '85.00',
+        nome: "Volume Luxo",
+        foto: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/luxo_glamour.jpeg",
+        nota: "5.0",
+        valor: "140.00",
+        descricao:
+          "Esse método proporciona um volume médio a intenso, criando um olhar marcante e glamoroso.",
+        manutencao: "90.00",
         maisVendido: true,
-        story: ''
+        story: "",
       },
       {
-        nome: 'Volume Egípcio',
-        foto: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/01.PNG',
-        nota: '5.0',
-        valor: '125.00',
-        descricao: 'Um efeito natural e delicado. São ideais para quem busca um olhar marcante e suave.',
-        manutencao: '80.00',
+        nome: "Volume Egípcio",
+        foto: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/egip.jpeg",
+        nota: "5.0",
+        valor: "125.00",
+        descricao:
+          "Um efeito natural e delicado. São ideais para quem busca um olhar marcante e suave.",
+        manutencao: "85.00",
         maisVendido: true,
-        story: ''
+        story: "",
       },
       {
-        nome: 'Volume Brasileiro',
-        valor: '110.00',
-        foto: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/4e.jpg',
-        nota: '5.0',
-        descricao: 'Cilíos um volume suave e natural, ideal para quem busca um olhar mais cheio, porém sem exageros.',
-        manutencao: '80.00',
+        nome: "Volume Brasileiro",
+        valor: "110.00",
+        foto: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/gisele.jpeg",
+        nota: "5.0",
+        descricao:
+          "Cilíos um volume suave e natural, ideal para quem busca um olhar mais cheio, porém sem exageros.",
+        manutencao: "80.00",
         maisVendido: true,
-        story: ''
+        story: "",
       },
       {
-        nome: 'Lash Lifting',
-        foto: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/3e.jpg',
-        nota: '4.0',
-        valor: '80.00',
-        descricao: 'Cílios de volume natural e curvado, alongando e dando volume de forma sutil.',
-        manutencao: '',
+        nome: "Lash Lifting",
+        foto: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/l_lifting.jpeg",
+        nota: "4.0",
+        valor: "80.00",
+        descricao:
+          "Cílios de volume natural e curvado, alongando e dando volume de forma sutil.",
+        manutencao: "",
         maisVendido: false,
-        story: ''
+        story: "",
+      },
+            {
+        nome: "Volume Fox",
+        foto: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/fox.jpeg",
+        nota: "4.0",
+        valor: "150.00",
+        descricao: "Cílios com volume marcante, alongando o olhar e proporcionando destaque de forma natural.",
+        manutencao: "80.00",
+        maisVendido: false,
+        story: "",
       },
       {
-        nome: 'Hydra Lips',
-        foto: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/LIPS_.jpg',
-        nota: '4.0',
-        valor: '69.00',
-        descricao: 'Traga mais saúde para os seus lábios, revitalizando e hidratando de forma profunda.',
-        manutencao: '',
+        nome: "Micropigamentação Labial",
+        foto: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/1e.jpg",
+        nota: "5.0",
+        valor: "165.00",
+        descricao:
+          "Realce seus lábios com a Micropigmentação Labial, deixando seus lábios mais iluminados.",
         maisVendido: false,
-        story: ''
+        story: "",
       },
       {
-        nome: 'Micropigamentação Labial',
-        foto: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/1e.jpg',
-        nota: '5.0',
-        valor: '170.00',
-        descricao: 'Realce seus lábios com a Micropigmentação Labial, deixando seus lábios mais iluminados.',
+        nome: "Hydra Gloss",
+        foto: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/GLOOSS_.jpg",
+        nota: "4.0",
+        valor: "49.00",
+        descricao:
+          "Fique com os lábios mais jovens além efeito glossy, que deixa os lábios iluminados.",
+        manutencao: "",
         maisVendido: false,
-        story: ''
+        story: "",
       },
       {
-        nome: 'Hydra Gloss',
-        foto: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/GLOOSS_.jpg',
-        nota: '4.0',
-        valor: '49.00',
-        descricao: 'Fique com os lábios mais jovens além efeito glossy, que deixa os lábios iluminados.',
-        manutencao: '',
+        nome: "Curso de Lash iniciante",
+        foto: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/curso.jpeg",
+        nota: "5.0",
+        valor: "620.00",
+        descricao: "Curso iniciante de lash designer",
+        manutencao: "",
         maisVendido: false,
-        story: ''
+        story: "",
       },
     ],
 
     imagens: [
       {
-        src: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/6e.jpg',
-        mensagem: 'A extensão de cílios renova toda a sua beleza.'
+        src: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/new_cal.jpeg",
+        mensagem: "A extensão de cílios renova toda a sua beleza.",
       },
       {
-        src: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/02.jpg',
-        mensagem: 'Fio a fio a beleza se transforma.'
+        src: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/new_cal1.jpeg",
+        mensagem: "Fio a fio a beleza se transforma.",
       },
       {
-        src: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/03.jpg',
-        mensagem: 'Os seus olhos merecem cílios marcantes.'
+        src: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/new_cal2.jpeg",
+        mensagem: "Os seus olhos merecem cílios marcantes.",
       },
       {
-        src: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/BACK02.jpg',
-        mensagem: 'O seu olhar merece o cuidado de uma especialista em cílios. Renove o seu poder.'
+        src: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/new_cal3.jpeg",
+        mensagem:
+          "O seu olhar merece o cuidado de uma especialista em cílios. Renove o seu poder.",
       },
       {
-        src: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/BACK03.jpg',
-        mensagem: 'Charme e estética perfeita que só lindos cílios conseguem expressar.'
+        src: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/BACK03.jpg",
+        mensagem:
+          "Charme e estética perfeita que só lindos cílios conseguem expressar.",
       },
       {
-        src: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/BACK04.jpg',
-        mensagem: 'Cílios poderosos iluminam qualquer olhar.'
+        src: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/new_cal4.jpeg",
+        mensagem: "Cílios poderosos iluminam qualquer olhar.",
       },
       {
-        src: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/main/BACK05.jpg',
-        mensagem: 'Os cílios são a extensão do poder feminino. Cuide bem deles!'
+        src: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/new_cal5.jpeg",
+        mensagem:
+          "Os cílios são a extensão do poder feminino. Cuide bem deles!",
       },
       {
-        src: 'https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/egpc.jpeg',
-        mensagem: 'Agende seu horário!'
+        src: "https://raw.githubusercontent.com/WallaceHS20/IMAGE-REPOSITORY/refs/heads/main/new_cal6.jpeg",
+        mensagem: "Agende seu horário!",
       },
     ],
-    
-    phoneNumber: '5512996141491',
+
+    phoneNumber: "5512996141491",
   }),
 
-  actions: {
-    
-  }
-})
+  actions: {},
+});
